d1866aa50d50f999fa4e62f5e5231c080700d429
ii2Chris <ChrisBudhram03@outlook.com>
2025-10-30 14:37:57 -0400

    Added BlockType + Test Cases

---
+++ src/functions/block_to_block_type.py
@@ -0,0 +1,4 @@
+from .block_type import BlockType
+
+def block_to_block_type(block):
+    pass
--- src/functions/html_node.py
+++ src/functions/html_node.py
@@ -19,7 +19,6 @@ class HTMLNode:
 
 
 
-
 class LeafNode(HTMLNode):
     def __init__(self, tag=None, value=None, props=None):
         if value is None:
