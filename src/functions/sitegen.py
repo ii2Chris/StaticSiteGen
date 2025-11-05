b31881bfcfece47cc901f3d14d86886afaa350e6
ii2Chris <ChrisBudhram03@outlook.com>
2025-11-04 22:39:25 -0500

    generate pages works... except it doesn't work recursively

--- src/functions/sitegen.py
+++ src/functions/sitegen.py
@@ -1,2 +1,31 @@
+from .extract_markdown import extract_title
+from .block_to_html import markdown_to_html_node
+import os
+
 def generate_page(from_path, template_path, dest_path):
-    pass
+    print(f"Generating page from {from_path} using template {template_path} into {dest_path}")
+
+    # read the content and template files
+    with open(from_path, "r") as f:
+        content = f.read()
+    with open(template_path, "r") as f:
+        template = f.read()
+
+    # render HTML + extract title
+    node = markdown_to_html_node(content)
+    html = node.to_html()
+    title = extract_title(content)
+
+    # apply template
+    page = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+
+    # Ensure destination directory exists
+    dest_dir = os.path.dirname(dest_path)
+    if dest_dir:  # only create if not empty
+        os.makedirs(dest_dir, exist_ok=True)
+
+    with open(dest_path, "w") as f:
+        f.write(page)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    print(f"Generating pages recursively from {dir_path_content} using template {template_path} into {dest_dir_path}")
--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
+import os
 from functions.copy_static import copy_dir, prepare_public
-from functions.sitegen import generate_page
+from functions.sitegen import generate_page, generate_pages_recursive
 
 def main():
     static_path = "static"
     public_path = "public"
     content_md = "content/index.md"
-    template_path = "templates/index.html"
+    template_path = "template.html"
 
     prepare_public(public_path)
     copy_dir(static_path, public_path)
-    generate_page(content_md, template_path, public_path)
+
+    dest_html = os.path.join(public_path, "index.html")
+    generate_page(content_md, template_path, dest_html)
 
 if __name__ == "__main__":
     main()
